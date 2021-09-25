b3fc7b71dc5f51db230e0ec328bdf352b36aa1d0
Anna <k_anna_i@mail.ru>
2021-09-25 20:14:31 +0300

    creation

---
+++ src/js/GameController.js
@@ -0,0 +1,26 @@
+import themes from './themes';
+
+export default class GameController {
+  constructor(gamePlay, stateService) {
+    this.gamePlay = gamePlay;
+    this.stateService = stateService;
+  }
+
+  init() {
+    // TODO: add event listeners to gamePlay events
+    // TODO: load saved stated from stateService
+    GamePlay.drawUi(prairie);
+  }
+
+  onCellClick(index) {
+    // TODO: react to click
+  }
+
+  onCellEnter(index) {
+    // TODO: react to mouse enter
+  }
+
+  onCellLeave(index) {
+    // TODO: react to mouse leave
+  }
+}
