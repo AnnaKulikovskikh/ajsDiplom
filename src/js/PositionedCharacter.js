e4e7420b05feb234d479524c04aacb952201c1d1
Anna <k_anna_i@mail.ru>
2021-11-01 22:28:30 +0300

    011121

--- src/js/PositionedCharacter.js
+++ src/js/PositionedCharacter.js
@@ -12,5 +12,29 @@ export default class PositionedCharacter {
 
     this.character = character;
     this.position = position;
+    this.chargePositions = [];
   }
+
+  static getPosition(character) {
+    if (this.position == -1) {
+    //return occasion position for new character
+      let possible = [];
+      const boxSize = 8;
+      let s = 6;
+      if (character.type == "Bowman" || character.type == "Swordman" || character.type == "Magic") {
+        let s = 0;
+      }
+      for (let i = 0; i < boxSize; i ++) {
+        possible.push(s);
+        possible.push(s + 1);
+        s += boxSize;
+      }  
+      while (this.position == -1 || this.chargePositions.includes(this.position)) {
+        this.position = possible[Math.floor(Math.random() * possible.length)];
+      }
+      return {position: this.position, character: this.character};
+        
+    } //if new Character
+  } //getPosition
+
 }
